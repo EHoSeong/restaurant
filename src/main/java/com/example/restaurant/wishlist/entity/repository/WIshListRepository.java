63c91548132e10ebc34e0bbfbf314b112cdfb8c8
Jjangguisdryingclothes <lhs06185@nate.com>
2022-08-15 17:39:34 +0900

    Initial commit

---
+++ src/main/java/com/example/restaurant/wishlist/entity/repository/WIshListRepository.java
@@ -0,0 +1,10 @@
+package com.example.restaurant.wishlist.entity.repository;
+
+import com.example.restaurant.db.MemoryDbRepositoryAbstract;
+import com.example.restaurant.wishlist.entity.WishListEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WIshListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
+
+}
